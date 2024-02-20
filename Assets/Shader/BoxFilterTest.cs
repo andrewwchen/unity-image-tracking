bd3357dad41b5def009fbca72f41bb0ba8156e7e
Andrew W. Chen <f0040gy@dartmouth.edu>
2024-02-20 17:29:11 -0500

    compute shader work

--- Assets/Shader/BoxFilterTest.cs
+++ Assets/Shader/BoxFilterTest.cs
@@ -8,25 +8,81 @@ public class BoxFilterTest : MonoBehaviour
 {
     [SerializeField] private int boxFilterKernel = 5;
     [SerializeField] private bool useSecondaryCamera = false;
-    [SerializeField] private ComputeShader computeShader;
-    [SerializeField] private RenderTexture arCameraBackgroundRenderTexture;
-    [SerializeField] private RenderTexture boxFilterRenderTexture;
+    [SerializeField] private ComputeShader rgba2Gray;
+    [SerializeField] private ComputeShader boxFilter;
+    [SerializeField] private ComputeShader gray2Rgba;
     [SerializeField] private Camera secondaryCamera;
+    [SerializeField] private Texture2D markerImg;
+
     private Camera primaryCamera;
     private ARCameraBackground arCameraBackground;
+    private RenderTexture rgbaMarkerRT;
+    private RenderTexture grayMarkerRT;
+    private RenderTexture rgbaRT;
+    private RenderTexture grayRT;
+    private RenderTexture grayBoxRT;
+    private RenderTexture rgbaBoxRT;
+    private int width;
+    private int height;
+    private int depth;
+    private int scale = 2;
     // Start is called before the first frame update
     void Start()
     {
         primaryCamera = GetComponent<Camera>();
         arCameraBackground = GetComponent<ARCameraBackground>();
-        arCameraBackgroundRenderTexture = new RenderTexture(primaryCamera.pixelWidth, primaryCamera.pixelHeight, 24);
-        arCameraBackgroundRenderTexture.enableRandomWrite = true;
-        arCameraBackgroundRenderTexture.Create();
+        width = primaryCamera.pixelWidth/ scale;
+        height = primaryCamera.pixelHeight/ scale;
+        depth = 1;
+
 
         if (useSecondaryCamera)
         {
-            secondaryCamera.targetTexture = arCameraBackgroundRenderTexture;
+            width = secondaryCamera.pixelWidth/ scale;
+            height = secondaryCamera.pixelHeight/ scale;
         }
+
+        rgbaRT = new RenderTexture(width, height, depth);
+        rgbaRT.enableRandomWrite = true;
+        rgbaRT.Create();
+
+        grayRT = new RenderTexture(width, height, depth);
+        grayRT.enableRandomWrite = true;
+        grayRT.Create();
+
+        grayBoxRT = new RenderTexture(width, height, depth);
+        grayBoxRT.enableRandomWrite = true;
+        grayBoxRT.Create();
+
+        rgbaBoxRT = new RenderTexture(width, height, depth);
+        rgbaBoxRT.enableRandomWrite = true;
+        rgbaBoxRT.Create();
+
+
+        if (useSecondaryCamera)
+        {
+            secondaryCamera.targetTexture = rgbaRT;
+        }
+
+        int markerWidth = markerImg.width;
+        int markerHeight = markerImg.height;
+
+        rgbaMarkerRT = new RenderTexture(markerWidth, markerHeight, depth);
+        rgbaMarkerRT.enableRandomWrite = true;
+        rgbaMarkerRT.Create();
+
+        grayMarkerRT = new RenderTexture(markerWidth, markerHeight, depth);
+        grayMarkerRT.enableRandomWrite = true;
+        grayMarkerRT.Create();
+
+        RenderTexture.active = rgbaMarkerRT;
+        Graphics.Blit(markerImg, rgbaMarkerRT);
+
+        rgba2Gray.SetTexture(0, "Input", rgbaMarkerRT);
+        rgba2Gray.SetTexture(0, "Result", grayMarkerRT);
+        rgba2Gray.Dispatch(0, markerWidth, markerHeight, 1);
+
+
     }
 
     // Update is called once per frame
@@ -34,28 +90,29 @@ public class BoxFilterTest : MonoBehaviour
     {
         if (arCameraBackground.material != null)
         {
-            Graphics.Blit(null, arCameraBackgroundRenderTexture, arCameraBackground.material);
+            Graphics.Blit(null, rgbaRT, arCameraBackground.material);
         }
-        // Graphics.Blit(secondaryCamera.activeTexture, arCameraBackgroundRenderTexture);
-        // arCameraBackgroundRenderTexture = secondaryCamera.activeTexture;
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        if (boxFilterRenderTexture == null)
-        {
-            boxFilterRenderTexture = new RenderTexture(arCameraBackgroundRenderTexture.width, arCameraBackgroundRenderTexture.height, arCameraBackgroundRenderTexture.depth);
-            boxFilterRenderTexture.enableRandomWrite = true;
-            boxFilterRenderTexture.Create();
-        }
-        computeShader.SetTexture(0, "Input", arCameraBackgroundRenderTexture);
-        computeShader.SetTexture(0, "Result", boxFilterRenderTexture);
-        computeShader.SetInt("Width", arCameraBackgroundRenderTexture.width);
-        computeShader.SetInt("Height", arCameraBackgroundRenderTexture.height);
-        computeShader.SetInt("K", boxFilterKernel);
-        computeShader.Dispatch(0, boxFilterRenderTexture.width / 8, boxFilterRenderTexture.height / 8, 1);
+        rgba2Gray.SetTexture(0, "Input", rgbaRT);
+        rgba2Gray.SetTexture(0, "Result", grayRT);
+        rgba2Gray.Dispatch(0, width, height, 1);
+        
+        boxFilter.SetTexture(0, "Input", grayRT);
+        boxFilter.SetTexture(0, "Result", grayBoxRT);
+        boxFilter.SetInt("Width", width);
+        boxFilter.SetInt("Height", height);
+        boxFilter.SetInt("K", boxFilterKernel);
+        boxFilter.Dispatch(0, width, height, 1);
+        
+        gray2Rgba.SetTexture(0, "Input", grayBoxRT);
+        gray2Rgba.SetTexture(0, "Result", rgbaBoxRT);
+        gray2Rgba.Dispatch(0, width, height, 1);
 
-        Graphics.Blit(boxFilterRenderTexture, destination);
+        Graphics.Blit(rgbaBoxRT, destination);
+        // Graphics.Blit(grayMarkerRT, destination);
     }
 
     public void IncrementBoxFilterKernel(int n)
